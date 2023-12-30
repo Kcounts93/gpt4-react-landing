06c671f198e94950bc48a5657f12b95505b524b1
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-12-30 14:07:47 -0800

    cta component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./cta.css";
+import { RiArrowRightUpLine } from "react-icons/ri";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt4__cta">
+      <div className="gpt4__cta-content">
+        <p>Begin using GPT-4</p>
+        <h3>Sign up & start exploring the endless possibilities with GPT-4</h3>
+      </div>
+      <div className="gpt4__cta-btn">
+        <button type="button">
+          Get started
+          <RiArrowRightUpLine size={20} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
