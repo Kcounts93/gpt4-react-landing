bdf2cdff9781b292b58d445f7d257d381ef4c67e
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-12-30 11:20:47 -0800

    brand update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 
 import { Footer, Blog, About, Header, Features, Demogpt } from "./containers";
-import { CTA, Brand, Navbar } from "./components";
+import { CTA, Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <Brand />
       <About />
       <Features />
       <Demogpt />
--- src/components/brand/imports.js
+++
@@ -1,7 +0,0 @@
-import climate from "../../assets/climate-policy.svg";
-import dropbox from "../../assets/dropbox-white.svg";
-import harvey from "../../assets/harvey.svg";
-import material from "../../src/assets/material.svg";
-import mem from "../../assets/mem-white.svg";
-
-export { climate, dropbox, harvey, material, mem };
