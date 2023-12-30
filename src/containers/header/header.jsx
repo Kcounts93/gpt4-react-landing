5d3345ca98aecb787b49563a9d9145a907336750
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-12-30 10:49:08 -0800

    header component/nav hover

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import "./header.css";
+import ai from "../../assets/gpt4-hero.png";
+import { RiArrowRightUpLine } from "react-icons/ri";
 
 const header = () => {
-  return <div>header</div>;
+  return (
+    <div className="gpt4__header section__padding" id="home">
+      <div className="gpt4__header-content">
+        <p>Chat GPT-4</p>
+        <h1 className="gradient__text">
+          OpenAI’s most advanced system, producing safer and more useful
+          responses
+        </h1>
+        <p>
+          GPT-4 has the capability to solve complex and challenging problems
+          with enhanced precision and efficiency, thanks to its expansive
+          general knowledge base and advanced problem-solving abilities, which
+          enable it to understand and interpret a wide range of topics more
+          effectively.
+        </p>
+
+        <div className="gpt4__header-buttons">
+          <button type="button">
+            Try ChatGPT
+            <RiArrowRightUpLine size={20} />
+          </button>
+          <p>
+            <a href="#features" className="button-secondary">
+              View features
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div className="gpt4__header-image">
+        <img src={ai} />
+      </div>
+    </div>
+  );
 };
 
 export default header;
