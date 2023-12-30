e8ee1eae69df6697fa38bbe6439e6307c586e9d5
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-12-30 14:25:26 -0800

    bog container and article components

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import Article from "../../components/article/Article";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt4__blog section__padding" id="blog">
+      <div className="gpt4__blog-heading">
+        <h1 className="gradient__text">Latest updates</h1>
+      </div>
+      <div className="gpt4__blog-container">
+        <div className="gpt4__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="Nov 29, 2023"
+            text="Sam Altman returns as CEO, OpenAI has a new initial board"
+          />
+        </div>
+        <div className="gpt4__blog-container_groupB">
+          <Article imgUrl={blog02} date="Nov 6, 2023" text="Introducing GPTs" />
+          <Article
+            imgUrl={blog03}
+            date="Nov 6, 2023"
+            text="New models and developer products announced at DevDay"
+          />
+          <Article
+            imgUrl={blog04}
+            date="Oct 19,2023"
+            text="DALL·E 3 is now available in ChatGPT Plus and Enterprise"
+          />
+          <Article
+            imgUrl={blog05}
+            date="Sep 25, 2023"
+            text="ChatGPT can now see, hear, and speak"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,7 @@
+import blog01 from "../../assets/blog01.avif";
+import blog02 from "../../assets/blog02.avif";
+import blog03 from "../../assets/blog03.avif";
+import blog04 from "../../assets/blog04.avif";
+import blog05 from "../../assets/blog05.avif";
+
+export { blog01, blog02, blog03, blog04, blog05 };
