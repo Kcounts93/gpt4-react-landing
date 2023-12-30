9688074850ece481d6e8f5b51ae6a6791520638b
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-12-30 11:03:05 -0800

    brand component assets

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { dropbox, harvey, mem, climate, material } from "./import.js";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  <div className="gpt4__brand section__padding">
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={harvey} />
+    </div>
+    <div>
+      <img src={mem} />
+    </div>
+    <div>
+      <img src={climate} />
+    </div>
+    <div>
+      <img src={material} />
+    </div>
+  </div>;
 };
 
 export default Brand;
